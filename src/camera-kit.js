86a7a0f9610ad5b27c945a48ddab9f6422c7d6b3
Michael Turkington <62445676+Michael-Turkington@users.noreply.github.com>
2024-03-30 12:59:16 +0000

    Update camera-kit.js

--- src/camera-kit.js
+++ src/camera-kit.js
@@ -2,6 +2,7 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/
 
 window.addEventListener("load", (event) => {
         console.log("Hello");
+        console.log("Hello2");
         
     const apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzExNjAxNjAyLCJzdWIiOiIyY2ZkMzFiZC1lMWFmLTQ1N2QtYWJmYy1hOThhNzkzMzRlMWZ-U1RBR0lOR34wOTM2YTE4NC00NjJiLTQ5YjgtOGZjYi05YzAwMDNmMmUwNzUifQ.xEpM3aiqXNU6uIM6w70BfsWag42WdXt3wnGv-hckOUk";
     const cameraKit = await bootstrapCameraKit({ apiToken });
